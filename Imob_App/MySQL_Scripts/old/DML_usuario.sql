f8c44a094db9a4305b9b721ab7178f31a33ddff9
Leonardo Cesar Rombesso <lrombesso@gmail.com>
2012-08-26 01:42:32 -0300

    Migracao para repositorio privado
    Se você não tiver recebido o e-mail do novo repositório, me ligue, por
    favor.

--- Imob_App/MySQL_Scripts/old/DML_usuario.sql
+++
@@ -1,5 +0,0 @@
-USE ImobApp;
-
-INSERT INTO usuario VALUES
-(123456, 'Guilherme Andrade', '38570535856', '343534', 'batera74@hotmail.com', '1397897897',
-'Residencial', '1378947897', 'Celular', 1, 'Ativo')
--- Imob_App/imob_app.client/Imagem.ashx
+++
@@ -1,1 +0,0 @@
-﻿<%@ WebHandler Language="C#" CodeBehind="Imagem.ashx.cs" Class="imob_app.client.Imagem" %>
